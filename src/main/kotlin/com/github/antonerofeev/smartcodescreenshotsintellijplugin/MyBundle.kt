e20142c9bbbdaffb9e089e36340ab2f611ac0a24
GitHub Action <action@github.com>
2025-08-31 15:24:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/antonerofeev/smartcodescreenshotsintellijplugin/MyBundle.kt
+++ src/main/kotlin/com/github/antonerofeev/smartcodescreenshotsintellijplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.antonerofeev.smartcodescreenshotsintellijplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
